42fc31ba3a16c3529f47e1b464699dd1a9f40e0b
Noman <pxycknomdictator@outlook.com>
2024-12-17 21:42:06 +0500

    more data types

--- rust_types/src/main.rs
+++ rust_types/src/main.rs
@@ -4,6 +4,8 @@ fn main() {
     // string literals
     let username: &str = "John Doe";
 
+    println!("My name is: {}", username);
+
     /*
         Numbers (Signed && Unsigned)
         signed for positive and negative integers
@@ -13,6 +15,24 @@ fn main() {
     let street: i32 = -90;
     let max_number: usize = 909999999999999;
 
-    println!("{}, {}, {}, {}", username, age, street, max_number);
+    println!(
+        "My age is: {} and street number: {} and longest number {}",
+        age, street, max_number
+    );
+    // floats values
+
+    const PI: f32 = 3.14;
+    let my_float_number: f64 = 90.3884939484843;
+    let floating_number: f32 = 83.29894;
+
+    println!(
+        "The value of PI is: {}, longest floating number {}, and smallest floating number {}",
+        PI, my_float_number, floating_number
+    );
+
+    // booleans aka bool in rust
+
+    let is_programmer: bool = true;
+    println!("I am a programmer ? {}", is_programmer);
 
 }
