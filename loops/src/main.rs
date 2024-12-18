ecf79db91772b991bac2bc59c4821aee514203e5
Noman <pxycknomdictator@outlook.com>
2024-12-19 00:56:42 +0500

    loops in rust

---
+++ loops/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    /* so what is loop i don't need to explain
+        There are 3 types of loops in Rust majorly
+        ["for", "while", "loop"]
+    */
+
+    // If you hate your self use this code and break your pc
+    // let mut counter: i32 = 0;
+
+    // loop {
+    //     println!("Oh no! {}", counter);
+    //     counter += 1;
+    // }
+}
