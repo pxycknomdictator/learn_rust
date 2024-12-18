ce06474ceceacae9d607d0a2ea7f1a2e3dd64ef4
Noman <pxycknomdictator@outlook.com>
2024-12-18 22:04:12 +0500

    add if/else ladder statement

--- control_flow/src/main.rs
+++ control_flow/src/main.rs
@@ -6,4 +6,22 @@ fn main() {
     } else {
         println!("You are authenticate person");
     }
+
+    let day: &str = "Friday";
+
+    if day == "Monday" {
+        println!("First day is: {}", day);
+    } else if day == "Tuesday" {
+        println!("Second day is: {}", day);
+    } else if day == "Wednesday" {
+        println!("Third day is: {}", day);
+    } else if day == "Thursday" {
+        println!("Fourth day is: {}", day);
+    } else if day == "Friday" {
+        println!("Fifth day is: {}", day);
+    } else if day == "Saturday" {
+        println!("Sixth day is: {}", day);
+    } else {
+        println!("Okay so the day is: {}", day);
+    }
 }
