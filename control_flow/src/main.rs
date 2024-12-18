6239f66fd74821ffa1eb7b8735970aefda0ba3f3
Noman <pxycknomdictator@outlook.com>
2024-12-18 21:58:52 +0500

    add if/else statement

---
+++ control_flow/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let is_authentication: bool = true;
+
+    if !is_authentication {
+        println!("You are not authentication");
+    } else {
+        println!("You are authenticate person");
+    }
+}
