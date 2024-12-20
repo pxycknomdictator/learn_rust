837cd4843aa755fc3595d1a9a7b72344cb17ee59
Noman <pxycknomdictator@outlook.com>
2024-12-20 13:22:36 +0500

    celsius to fahrenheit converter

--- function/src/main.rs
+++ function/src/main.rs
@@ -7,9 +7,16 @@ fn main() {
     let student1: f64 = calculate_and_return(200, 147);
     println!("Student1 percentage is: {}", student1);
     add_two_numbers(90, 18);
+
+    let temp: f64 = celsius_to_fahrenheit(38);
+    println!("Fahrenheit is: {}", temp.round());
 }
 
 fn add_two_numbers(num1: i32, num2: i32) {
     let result: i32 = num1 + num2;
     println!("{} + {} = {}", num1, num2, result);
+}
+
+fn celsius_to_fahrenheit(celsius: i32) -> f64 {
+    1.8 * celsius as f64 + 32.0
 }
