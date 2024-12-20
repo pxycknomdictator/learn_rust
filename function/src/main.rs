fb1b7d3eb564502ac5bedd4e8cd3fad1710aa3e8
Noman <pxycknomdictator@outlook.com>
2024-12-20 13:12:41 +0500

    add percentage cal fn

--- function/src/main.rs
+++ function/src/main.rs
@@ -1,5 +1,11 @@
+fn calculate_and_return(n1: i32, n2: i32) -> f64 {
+    return (n2 as f64 / n1 as f64) * 100.0;
+}
+
 fn main() {
     println!("Hello Rust Functions");
+    let student1: f64 = calculate_and_return(200, 147);
+    println!("Student1 percentage is: {}", student1);
     add_two_numbers(90, 18);
 }
 
