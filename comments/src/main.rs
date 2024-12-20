157f1c53466d14ce45128f8a220d9986e47e359c
Noman <pxycknomdictator@outlook.com>
2024-12-20 16:28:18 +0500

    add comments

---
+++ comments/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+
+    // I don't know why i am doing this seriously???
+
+    // Comments is a statement or message for you to explain what does this code do
+
+    println!("Comments");
+}
