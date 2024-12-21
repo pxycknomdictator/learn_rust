2a58db61c51afd89cfdafc9c822d4460ec1ef77d
Noman <pxycknomdictator@outlook.com>
2024-12-21 22:36:55 +0500

    add basics of ownership in rust

---
+++ ownership/src/main.rs
@@ -0,0 +1,33 @@
+fn main() {
+    // Ownership is the hardest and Final boss concept in Rust lets checkout how much hard is that
+
+    {
+        let _s = "Hello world"; // scope start from here
+    } // scope end here
+
+    // here values are copied
+    let mut student_1: &str = "Registration Form";
+    let student_2: &str = student_1;
+
+    student_1 = "School Leaving certificate";
+
+    println!("student 1: {} ", student_1);
+    println!("student 2: {} ", student_2);
+
+    // same values are copied
+    let mut age: i32 = 20;
+    let age2: i32 = age;
+
+    age = 90;
+
+    println!("Age: {}", age);
+    println!("Age2: {}", age2);
+
+    // what about special && complex data types like String
+
+    let mut todo: String = String::from("Hello, ");
+
+    todo.push_str("word");
+
+    println!("{todo}");
+}
