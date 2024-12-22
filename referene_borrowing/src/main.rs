03dbc845dd46201aed98c351e8750d86a26c577c
Noman <pxycknomdictator@outlook.com>
2024-12-22 15:57:18 +0500

    add reference with immutable variables

--- referene_borrowing/src/main.rs
+++ referene_borrowing/src/main.rs
@@ -6,12 +6,24 @@ fn main() {
         First i give ownership to calculate_length and he calculate the length or name and return a length and name ownership back
     */
 
+    // let name: String = String::from("John Doe");
+    // let (name, length): (String, usize) = calculate_length(name);
+    // println!("My name is: {} and my name length is: {}", name, length);
+
+    // =========================================================================================================
+
+    // when i put & symbol thats mean i give you a reference (borrow this immutable variable and read only) this make our life easier
     let name: String = String::from("John Doe");
-    let (name, length): (String, usize) = calculate_length(name);
+    let length: usize = calculate_length(&name);
     println!("My name is: {} and my name length is: {}", name, length);
 }
 
-fn calculate_length(name: String) -> (String, usize) {
+fn calculate_length(name: &String) -> usize {
     let length: usize = name.len();
-    (name, length)
+    length
 }
+
+// fn calculate_length(name: String) -> (String, usize) {
+//     let length: usize = name.len();
+//     (name, length)
+// }
