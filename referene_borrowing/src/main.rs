882128f4cc0ea031971c87c9d73f26719659a46c
Noman <pxycknomdictator@outlook.com>
2024-12-22 15:50:37 +0500

    add hard way to job done

--- referene_borrowing/src/main.rs
+++ referene_borrowing/src/main.rs
@@ -1,5 +1,17 @@
 fn main() {
-    let mut name: String = String::from("John");
-    name.push_str(" Doe");
-    println!("My name is: {}", name);
+    // so lets talk about what is Borrowing or references in RUST
+
+    /*
+        what is going on here?
+        First i give ownership to calculate_length and he calculate the length or name and return a length and name ownership back
+    */
+
+    let name: String = String::from("John Doe");
+    let (name, length): (String, usize) = calculate_length(name);
+    println!("My name is: {} and my name length is: {}", name, length);
+}
+
+fn calculate_length(name: String) -> (String, usize) {
+    let length: usize = name.len();
+    (name, length)
 }
