f8e96aeaac04a50a7d3b8f4175cca79fc75be7a4
Noman <pxycknomdictator@outlook.com>
2025-01-01 04:55:42 +0500

    understanding slices in rust

---
+++ slices/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    // After a long time of learning, we are finally here This topic will be about slices and this make me sick
+
+    let greetings: String = String::from("Hello Rust");
+    println!("{}", greetings);
+}
