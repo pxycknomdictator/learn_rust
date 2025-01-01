52e0fc259d4e5100eefddb7757e5eb8fdccc7b76
Noman <pxycknomdictator@outlook.com>
2025-01-02 03:10:51 +0500

    add multiple instances

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,3 +1,5 @@
+// structs are used to create custom data types in Rust that can help us to create multiple instances of a struct with different values. just like classes in other programming languages. This is look like interface in TypeScript and create multiple instances  with different values.
+
 struct User {
     username: String,
     age: i32,
@@ -6,6 +8,7 @@ struct User {
 }
 
 fn main() {
+    // Custom data types han? remind me of interfaces in TypeScript :D
     let first_user: User = User {
         jwt_token: String::from("9922iks9o293901"),
         age: 92,
@@ -17,5 +20,17 @@ fn main() {
     print!("{} \n", first_user.username);
     print!("{} \n", first_user.age);
     print!("{} \n", first_user.jwt_token);
+
+    let second_user: User = User {
+        age: 92,
+        jwt_token: String::from("992kd82992929"),
+        username: String::from("John Doe"),
+        active: false
+    };
+
+    print!("{} \n", second_user.active);
+    print!("{} \n", second_user.username);
+    print!("{} \n", second_user.age);
+    print!("{} \n", second_user.jwt_token);
 
 }
