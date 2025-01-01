01bd0f2c6a9e41c1b1489adeae9b935e18d6634d
Noman <pxycknomdictator@outlook.com>
2025-01-01 05:13:05 +0500

    understanding the bug

--- slices/src/main.rs
+++ slices/src/main.rs
@@ -1,10 +1,13 @@
 fn main() {
     // After a long time of learning, we are finally here This topic will be about slices and this make me sick
 
-    let greetings: String = String::from("Hello Rust");
+    // So whats happening here is that i have a string and i want to get the first word of it and i'm using a function to do that and i'm passing the string as a reference to the function after returning the function i clear original string and print it and the result of the function I just calculate the length and result is the calculated length of the first word of the string
 
-    // here we get a particular word from the string :)
+    let mut greetings: String = String::from("Hello Rust");
     let result =  get_first_word(&greetings);
+
+    greetings.clear(); // here i clear the string and make some unexpected result
+
     println!("The string: {} world length is: {}", greetings, result);
 }
 
