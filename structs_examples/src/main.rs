428abeef5b04d09e3bc13b8fc582178362884884
Noman <pxycknomdictator@outlook.com>
2025-01-05 16:52:11 +0500

    add debugging structs

--- structs_examples/src/main.rs
+++ structs_examples/src/main.rs
@@ -1,7 +1,9 @@
 // # Here is the most cleaner and updated version of creating rectangle with structs
+
+#[derive(Debug)] // this is line is help us to debug struct values
 struct Rectangle {
     width: i32,
-    height: i32
+    height: i32,
 }
 
 fn main() {
@@ -9,10 +11,16 @@ fn main() {
 
     let rectangle: Rectangle = Rectangle {
         width: 30,
-        height: 50
+        height: 50,
     };
 
-    println!("The area of the rectangle is: {} pixels", calculate_area(&rectangle)); // Giving a reference or borrow to calculate_area because we don't want to loose ownership
+    println!(
+        "The area of the rectangle is: {} pixels",
+        calculate_area(&rectangle)
+    );
+
+    // println!("{}", rectangle); // This line does not work!
+    println!("{rectangle:?}"); // In this way we use pretty way to display
 }
 
 fn calculate_area(rectangle: &Rectangle) -> i32 {
