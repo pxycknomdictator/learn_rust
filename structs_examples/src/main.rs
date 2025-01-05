26581ccd802630304ceecd42973070ddab2470f5
Noman <pxycknomdictator@outlook.com>
2025-01-05 16:23:48 +0500

    add clear version of calculating area with tuple

--- structs_examples/src/main.rs
+++ structs_examples/src/main.rs
@@ -1,11 +1,9 @@
 fn main() {
-    let width = 30;
-    let height = 50;
-
-    println!("The area of the rectangle is: {} pixels", calculate_area(width, height));
+    // # Here is the updated and clear version of calculating area by tuple
+    let rectangle: (i32, i32) = (30, 50);
+    println!("The area of the rectangle is: {} pixels", calculate_area(rectangle));
 }
 
-
-fn calculate_area(width: i32, height: i32) -> i32 {
-    width * height
+fn calculate_area(dimension: (i32, i32)) -> i32 {
+    dimension.0 * dimension.1
 }
