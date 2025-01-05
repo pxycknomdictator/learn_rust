485c872628926ded4434a0c97f414e553da5a7b6
Noman <pxycknomdictator@outlook.com>
2025-01-05 16:30:37 +0500

    add most clear version with structs

--- structs_examples/src/main.rs
+++ structs_examples/src/main.rs
@@ -1,9 +1,20 @@
-fn main() {
-    // # Here is the updated and clear version of calculating area by tuple
-    let rectangle: (i32, i32) = (30, 50);
-    println!("The area of the rectangle is: {} pixels", calculate_area(rectangle));
+// # Here is the most cleaner and updated version of creating rectangle with structs
+struct Rectangle {
+    width: i32,
+    height: i32
 }
 
-fn calculate_area(dimension: (i32, i32)) -> i32 {
-    dimension.0 * dimension.1
+fn main() {
+    // creating rectangle instance
+
+    let rectangle: Rectangle = Rectangle {
+        width: 30,
+        height: 50
+    };
+
+    println!("The area of the rectangle is: {} pixels", calculate_area(&rectangle)); // Giving a reference or borrow to calculate_area because we don't want to loose ownership
+}
+
+fn calculate_area(rectangle: &Rectangle) -> i32 {
+    rectangle.width * rectangle.height
 }
