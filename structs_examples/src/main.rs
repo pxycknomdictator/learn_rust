5b20c4de654899f9209f0588108d6302c8f6c523
Noman <pxycknomdictator@outlook.com>
2025-01-06 21:49:28 +0500

    add overview of method syntax

--- structs_examples/src/main.rs
+++ structs_examples/src/main.rs
@@ -1,28 +1,51 @@
-// # Here is the most cleaner and updated version of creating rectangle with structs
+// // # Here is the most cleaner and updated version of creating rectangle with structs
 
-#[derive(Debug)] // this is line is help us to debug struct values
+// #[derive(Debug)] // this is line is help us to debug struct values
+// struct Rectangle {
+//     width: i32,
+//     height: i32,
+// }
+
+// fn main() {
+//     // creating rectangle instance
+
+//     let rectangle: Rectangle = Rectangle {
+//         width: 30,
+//         height: 50,
+//     };
+
+//     println!(
+//         "The area of the rectangle is: {} pixels",
+//         calculate_area(&rectangle)
+//     );
+
+//     // println!("{}", rectangle); // This line does not work!
+//     println!("{rectangle:?}"); // In this way we use pretty way to display
+// }
+
+// fn calculate_area(rectangle: &Rectangle) -> i32 {
+//     rectangle.width * rectangle.height
+// }
+
+// let's learn about method syntax
 struct Rectangle {
-    width: i32,
-    height: i32,
+    width: u32,
+    height: u32,
+}
+
+// so what the hell i am doing here? as a javascript dev i think we are creating a methods for Rectangle structs and here self just like (this) keyword in JS
+
+impl Rectangle {
+    fn calculate_area(&self) -> u32 {
+        self.width * self.height
+    }
 }
 
 fn main() {
-    // creating rectangle instance
-
-    let rectangle: Rectangle = Rectangle {
-        width: 30,
-        height: 50,
+    let rec1 = Rectangle {
+        width: 89,
+        height: 49,
     };
 
-    println!(
-        "The area of the rectangle is: {} pixels",
-        calculate_area(&rectangle)
-    );
-
-    // println!("{}", rectangle); // This line does not work!
-    println!("{rectangle:?}"); // In this way we use pretty way to display
-}
-
-fn calculate_area(rectangle: &Rectangle) -> i32 {
-    rectangle.width * rectangle.height
+    println!("area is: {}", rec1.calculate_area());
 }
