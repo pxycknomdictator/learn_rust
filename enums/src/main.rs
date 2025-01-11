3882790ad6ce804f74acdb5074fe280c6a2dd008
Noman <pxycknomdictator@outlook.com>
2025-01-11 23:46:24 +0500

    add a special not to understand

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -23,14 +23,14 @@ fn main() {
     let version4 = IpAddressTypes::V4(String::from("192.168.1.1"));
     let version6 = IpAddressTypes::V6(String::from("::1"));
 
+    // just like switch statement but its not so easy to read and understand
     match version4 {
         IpAddressTypes::V4(addr) => println!("Ip version is: {}", addr),
-        IpAddressTypes::V6(addr) => println!("Ip version is: {}", addr)
+        IpAddressTypes::V6(addr) => println!("Ip version is: {}", addr),
     }
 
     match version6 {
         IpAddressTypes::V4(addr) => println!("{}", addr),
         IpAddressTypes::V6(addr) => println!("{}", addr),
     }
-
 }
