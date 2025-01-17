c908b3e6c38338a83552ed87e762fc870696ab8d
Noman <pxycknomdictator@outlook.com>
2025-01-17 15:43:23 +0500

    added rand library for create random numbers

--- cli_game/src/main.rs
+++ cli_game/src/main.rs
@@ -1,8 +1,11 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
+    // This will generate random number from 1 to 100 inclusive
+
+    let number = rand::thread_rng().gen_range(1..=100);
     let mut input_number = String::new();
-    let number = 9;
 
     println!("Welcome to guessing Game");
     println!("Enter your number");
