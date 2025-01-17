95cb8332c3725e3d6f1c95f14efa2c384e1346c9
Noman <pxycknomdictator@outlook.com>
2025-01-17 16:21:11 +0500

    completed a number guessing game

--- cli_game/src/main.rs
+++ cli_game/src/main.rs
@@ -1,30 +1,41 @@
 use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     // This will generate random number from 1 to 100 inclusive
     let number: u8 = rand::thread_rng().gen_range(1..=100);
+    println!("Welcome to guessing Game");
+    println!("Enter your number");
 
-    'game_loop: loop {
-        println!("Welcome to guessing Game");
-        println!("Enter your number");
-
+    loop {
         let mut input_number = String::new();
 
         io::stdin()
             .read_line(&mut input_number)
             .expect("Failed to get Input");
 
-        // here i use shadowing concept && parsing input string to number
-        let input_number: u8 = input_number.trim().parse().expect("Failed to parse");
-        println!("Secret Number was: {}\nYou Guess: {}", number, input_number);
+        // here i use shadowing concept && parsing input string to number and handling error when user enter a string
+        let input_number: u8 = match input_number.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a valid number!");
+                continue;
+            }
+        };
+
+        let output: &str = match input_number.cmp(&number) {
+            Ordering::Less => "To Small!",
+            Ordering::Greater => "To Big!",
+            Ordering::Equal => {
+                println!("You Win! The number is: {}", number);
+                break;
+            }
+        };
 
         if input_number != number {
+            println!("{}", output);
             continue;
-        }
-
-        if input_number == number {
-            break 'game_loop;
         }
     }
 }
