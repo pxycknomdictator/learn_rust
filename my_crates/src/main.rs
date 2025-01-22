3f28f94343e084ee276d89fe69b3ff1b1a0100b0
Noman <pxycknomdictator@outlook.com>
2025-01-22 16:10:32 +0500

    introduction of crates and modules

---
+++ my_crates/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    // Crates and modules in rust
+
+    /* 
+      Crates: Rust's packages, which can be binary (executable programs) or library (reusable code).
+      Modules: Used to organize code within a crate, making it reusable and easy to manage.
+      Default File: Rust runs main.rs by default for binary crates and lib.rs for library crates.
+      Purpose: These structures ensure clean, modular, and scalable Rust codebases. 
+    */
+
+    
+
+}
